581ddb459461fe5b8379e278d6ee2bda4ce32e41
Agrim2411 <Agrim.Jain@iiitb.org>
2019-10-14 14:38:47 +0530

    corrected if condition

---
+++ a.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{   int n;
+    scanf("%d",&n);
+    if(n==1918)
+       printf("26.09.1918");
+    if(n<1918)
+        {            if(n%4==0)
+                {
+                    printf("12.09.%d",n);
+                }
+            if(n%4!=0)
+                {
+                    printf("13.09.%d",n);
+                }
+        }
+    if(n>1918)
+        {
+            if((n%4==0 && n%100!=0) || n%400==0)
+            {
+                printf("12.09.%d",n);
+            }
+            else
+                printf("13.09.%d",n);
+        }
+
+    return 0;
+}
